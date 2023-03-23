ae1ef6ecc1a2661bbbfd9ba18014390529f500ef
SzymonWu <126473845+SzymonWu@users.noreply.github.com>
2023-03-23 16:46:21 +0100

    Add files via upload

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    pass1 = models.CharField(max_length=50)
+    pass2 = models.CharField(max_length=100)
--- authentication/views.py
+++ authentication/views.py
@@ -2,39 +2,47 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
 def home(request):
     return render(request, "authentication/index.html")
 
 def signup(request):
-
     if request.method == "POST":
-      #  username = request.POST.get('username')
-      username = request.POST['username']
-      fname = request.POST['fname']
-      lname = request.POST['lname']
-      email = request.POST['email']
-      pass1 = request.POST['pass1']
-      pass2 = request.POST['pass2']
-      
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
 
-      myuser = User.objects.create_user(username, email, pass1)
-      myuser = form.save(commit=False)
-      myuser.first_name = fname
-      myuser.last_name = lname
+        if pass1 != pass2:
+            messages.error(request, "Hasła nie są zgodne.")
+            return redirect('signup')
 
-      myuser.save()
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Nazwa użytkownika jest już zajęta.")
+            return redirect('signup')
 
-      messages.success(request, "Twoje konto zostało poprawnie założone.")
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Adres e-mail jest już zarejestrowany.")
+            return redirect('signup')
 
-      return redirect('signin')
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
 
+        messages.success(request, "Twoje konto zostało poprawnie założone.")
+        messages.success(request, "Możesz zalogować się na swoje konto")
+        return redirect('signin')
 
     return render(request, "authentication/signup.html")
 
 def signin(request):
+    
 
     if request.method == 'POST':
         username = request.POST['username']
@@ -44,15 +52,15 @@ def signin(request):
 
         if user is not None:
             login(request, user)
-            fname = user.fisrt_name
-            return render(request, "autehntication/index.html", {'fname': fname})
+            fname = user.first_name
+            return render(request, "authentication/index.html", {'fname': fname})
 
         else:
-            messages.error(request, "Złe Dane!")
-            return redirect('home')
+             messages.error(request, "Nieprawidłowa nazwa użytkownika lub hasło")
 
     return render(request, "authentication/signin.html")
 
 def signout(request):
-    pass
-    
+    logout(request)
+    messages.success(request, "Zostałeś poprawnie wylogowany")
+    return redirect('home')
