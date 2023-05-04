0dbf99cd13157e693aa67a89171125215908702b
Mlot00 <126474975+Mlot00@users.noreply.github.com>
2023-05-04 18:23:29 +0000

    Add files via upload

--- authentication/views.py
+++ authentication/views.py
@@ -1,95 +1,22 @@
-﻿import os
+﻿
+import os
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import login, logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+
 from studnot.settings import MEDIA_ROOT
-from .forms import AddUserForm, NoteForm
-from .models import MyGroup, Note, Category, User
+from .forms import NoteForm
+from .models import Note, Category
 from django.core.files.storage import FileSystemStorage
-from .forms import GroupForm
 
 
-@login_required
-def group_list(request):
-    groups = request.user.groups.all()
-    return render(request, 'authentication/group_list.html', {'groups': groups})
 
-def group_detail(request, pk):
-    group = get_object_or_404(MyGroup, pk=pk)
-    users = User.objects.filter(groups=group)
-    notes = Note.objects.filter(groups=group)
-    add_user_form = AddUserForm(group_id=pk, data=request.POST or None)
-    if request.method == 'POST' and add_user_form.is_valid():
-        add_user_form.save()
-        messages.success(request, 'Użytkownik został dodany do grupy.')
-        return redirect('group_detail', pk=pk)
-    return render(request, 'authentication/group_detail.html', {'group': group, 'users': users, 'notes': notes, 'add_user_form': add_user_form})
 
-@login_required
-def group_create(request):
-    myuser=request.user
-    if request.method == 'POST':
-        form = GroupForm(request.POST)
-        if form.is_valid():
-            group = form.save(commit=False)
-            group.owner = request.user
-            if MyGroup.objects.filter(name=group.name).exists():
-                messages.error(request, 'Grupa o takiej nazwie już istnieje.')
-                return redirect('group_create')
-            group.save()
-            myuser.groups.add(group)
-            return redirect('group_list')
-    else:
-        form = GroupForm()
-    return render(request, 'authentication/group_create.html', {'form': form})
-#@login_required
-#def add_user_to_group(request, group_id):
-    group = get_object_or_404(MyGroup, id=group_id)
-    if request.user in user.groups.all() and request.user != group.admin:
-        if request.method == 'POST':
-            form = AddUserForm(request.POST)
-            if form.is_valid():
-                user = form.cleaned_data['user']
-                user.groups.add(user)
-                messages.success(request, 'Użytkownik został dodany do grupy.')
-            return redirect('group_detail', group_id=group.id)
-    return render(request, 'authentication/add_user.html')
-
-@login_required
-def group_update(request, pk):
-    group = get_object_or_404(MyGroup, pk=pk)
-    if request.method == 'POST':
-        form = GroupForm(request.POST, instance=group)
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            if MyGroup.objects.filter(name=name).exclude(pk=group.pk).exists():
-                messages.error(request, 'Grupa o takiej nazwie już istnieje.')
-            else:
-                group = form.save()
-                return redirect('group_list')
-    else:
-        form = GroupForm(instance=group)
-    return render(request, 'authentication/group_update.html', {'form': form})
-
-@login_required
-def group_delete(request, pk):
-    group = get_object_or_404(MyGroup, pk=pk)
-
-    if request.method == 'POST':
-        group.delete()
-        return redirect('group_list')
-
-    return render(request, 'authentication/group_delete.html', {'group': group})
-    
-def remove_user_from_group(request, group_id, user_id):
-    group = get_object_or_404(MyGroup, id=group_id)
-    user = get_object_or_404(User, id=user_id)
-    if request.user == group.owner:
-        user.groups.remove(group)
-    return redirect('group_detail', group_id)
-
+# Create your views here.
 def home(request):
     return render(request, "authentication/index.html")
 
@@ -169,42 +96,50 @@ def signup(request):
                 'email': email,
             }
             return render(request, "authentication/signup.html", {'data': data})
-        
-        myuser = User.objects.create_user(username=username, email=email, password=pass1)
+
+
+        myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
         myuser.save()
+        
         messages.success(request, "Twoje konto zostało poprawnie założone")
-        group = MyGroup.objects.create(name=username, owner=myuser)
-        myuser.groups.add(group)
+            
+            
         return redirect('signin')
+            
+            
+    else:
+        form = NoteForm()
+        data = {
+            'username': '',
+            'fname': '',
+            'lname': '',
+            'email': '',
+            'pass1': '',
+            'pass2': ''
+        }
+        return render(request, "authentication/signup.html", {'form': form, 'data': data})
 
-    return render(request, "authentication/signup.html")
-            
-            
-    
 def signin(request):
     if request.method == 'POST':
         username = request.POST['username']
         pass1 = request.POST['pass1']
         
-        try:
-            user = User.objects.get(username=username)
-        except User.DoesNotExist:
-            user = None
+        user = authenticate(username=username, password=pass1)
         
-        if user is not None and user.check_password(pass1):
+        if user is not None:
             login(request, user)
-            username = user.username
+            fname = user.first_name
             messages.success(request, "Zalogowano poprawnie")
-            return render(request, "authentication/index.html",{"username":username})
+            return render(request, "authentication/index.html",{"fname":fname})
         else:
             messages.error(request, "Nieprawidłowa nazwa użytkownika lub hasło")
             return redirect('signin')
     
     return render(request, "authentication/signin.html")
 
-@login_required
+
 def signout(request):
     logout(request)
     messages.success(request, "Zostałeś poprawnie wylogowany")
@@ -217,7 +152,7 @@ def create_note_view(request):
         form = NoteForm(request.POST, request.FILES)
         if form.is_valid():
             note = form.save(commit=False)
-            note.owner = request.user
+            note.owner = request.user.username
             if 'picture' in request.FILES:
                 image_file = request.FILES['picture']
                 fs = FileSystemStorage()
@@ -225,12 +160,6 @@ def create_note_view(request):
                 uploaded_file_url = fs.url(filename)
                 note.picture = uploaded_file_url[6:]
             note.save()
-
-            group_id = request.POST.get('groups')
-            if group_id:
-                groups = MyGroup.objects.get(pk=group_id)
-                note.groups.add(groups)
-
             return redirect('notes_list')  # Redirect to notes list page after saving
     else:
         form = NoteForm()
@@ -240,22 +169,26 @@ def create_note_view(request):
 
 @login_required
 def notes(request):
-    notes = Note.objects.filter(owner = request.user.id)
+    notes = Note.objects.filter(owner = request.user.username)
     form = NoteForm()
     return render(request, 'authentication/notes.html', {'notes': notes, 'form': form})
 
 def notes_list(request):
-    notes = Note.objects.filter(owner=request.user.pk)
+    notes = Note.objects.filter(owner=request.user)
     context = {'notes': notes}
     return render(request, 'authentication/notes_list.html', context)
 
-@login_required
 def update_note_view(request, pk):
     note = get_object_or_404(Note, pk=pk)
 
     if request.method == 'POST':
         form = NoteForm(request.POST, request.FILES, instance=note)
         if form.is_valid():
+            picture_action = request.POST.get('picture_action')  # pobranie wartości przycisku wyboru akcji
+            if picture_action == 'delete' and note.picture:
+                note.picture.delete()  # usunięcie pliku z dysku
+                note.picture = None  # ustawienie wartości na None
+            note = form.save(commit=True)
             if not note.category:  # jeśli kategoria nie została wybrana w formularzu
                 note.category = note.category  # użyj domyślnej kategorii notatki
         note.save()
@@ -267,7 +200,7 @@ def update_note_view(request, pk):
 
 
 
-@login_required
+
 def delete_note_view(request, pk):
     note = get_object_or_404(Note, pk=pk)
     if request.method == 'POST':
